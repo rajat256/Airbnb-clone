dd012a121d91b6322a18210ed2af184c619e8e5e
Rajat Parkhe <rajatparkhe25@gmail.com>
2026-04-04 00:37:45 +0530

    remove src folder from airbnb clone

--- src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require('express');
-const authController = require('../controllers/authController');
-
-const router = Router();
-
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
-
-module.exports = router;
